9d1e5ec15af3cfba52e0d50075c7400490a934e4
MubashirLabar <mubashirnazir115@gmail.com>
2024-12-23 09:53:48 +0500

    tab navigation

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { View, StyleSheet } from "react-native";
 import { useFonts } from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import LoginScreen from "./screens/Login";
 import HomeScreen from "./screens/Home";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   console.log("App Screen");
@@ -25,15 +27,31 @@ const App = () => {
   return (
     <View style={style.app}>
       <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={
-            {
-              // headerShown: false,
-            }
-          }
-        >
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
+          <Tab.Screen
+            name="login"
+            component={LoginScreen}
+            // options={{
+            //   tabBarLabel: "Login",
+            //   tabBarIcon: () => <Icon name="home" color={color} size={size} />,
+            // }}
+          />
+          <Tab.Screen name="home" component={HomeScreen} />
+          <Tab.Screen name="products" component={HomeScreen} />
+          <Tab.Screen name="profile" component={HomeScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </View>
+  );
+
+  return (
+    <View style={style.app}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
           <Stack.Screen
-            options={{ title: "Login", headerShown: false }}
+            options={{
+              headerShown: false,
+            }}
             name="login"
             component={LoginScreen}
           />
